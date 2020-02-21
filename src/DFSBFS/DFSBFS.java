7f7eb0c1cbff1fadd983c5fa96d6a970c457bc67
Choi Young Hoon <c_yh0327@naver.com>
2020-02-21 21:56:55 +0900

    2020-02-21

---
+++ src/DFSBFS/DFSBFS.java
@@ -0,0 +1,167 @@
+package DFSBFS;
+
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Stack;
+
+class Queue<T>{
+
+    class Node<T>{
+        private T data;
+        private Node<T> next; // 다음 노드
+
+        public Node(T data){
+            this.data = data;
+        }
+    }
+
+    private Node<T> first;
+    private Node<T> last;
+
+    public void add(T item){
+        Node<T> t = new Node<T>(item);
+
+        if(last != null){
+            last.next =t;
+        }
+        last = t;
+        if(first == null){
+            first = last;
+        }
+    }
+
+    public T remove(){
+        if(first ==null){
+            throw new NoSuchElementException();
+        }
+        T data = first.data;
+        first = first.next;
+        if(first == null){
+            last = null;
+        }
+        return data;
+    }
+
+    public T peek(){
+        if(first ==null){
+            throw new NoSuchElementException();
+        }
+        return first.data;
+    }
+
+    public boolean isEmpty(){
+        return first==null;
+    }
+}
+
+class Graph{
+    class Node{
+        int data;
+        LinkedList<Node> adjacent; // 인접한 노드의 관계
+        boolean marked; //방문여부
+        Node (int data){ //노드의 생성자.
+            this.data = data; // 데이터 받고
+            this.marked = false; // false 로 초기화
+            adjacent = new LinkedList<Node>();
+        }
+    }
+    Node[] nodes;
+    Graph(int size){
+        nodes = new Node[size];
+        for(int i=0; i<size; i++){
+            nodes[i] = new Node(i);
+        }
+    }
+    void addEdge(int i1, int i2){ // 두 노드의 관계를 저장하는 함수
+        Node n1 = nodes[i1];
+        Node n2 = nodes[i2];
+        if(!n1.adjacent.contains(n2)){
+            n1.adjacent.add(n2);
+        }
+        if(!n2.adjacent.contains(n1)){
+            n2.adjacent.add(n1);
+        }
+    }
+    void dfs(){
+        dfs(0);
+    }
+
+    void dfs(int index){
+        Node root = nodes[index];
+        Stack<Node> stack = new Stack<Node>();
+        stack.push(root);
+        root.marked = true;
+        while(!stack.isEmpty()){
+            Node r= stack.pop();
+            for(Node n:r.adjacent){
+                if(n.marked==false){
+                    n.marked = true;
+                    stack.push(n);
+                }
+            }
+            visit(r);
+        }
+    }
+
+    void visit(Node n){
+        System.out.print(n.data + " ");
+    }
+
+    void bfs(){
+        bfs(0);
+    }
+
+    void bfs(int index){
+        Node root =nodes[index];
+        Queue<Node> queue = new Queue<Node>();
+        queue.add(root);
+        root.marked = true;
+        while(!queue.isEmpty()){
+            Node r = queue.remove();
+            for(Node n : r.adjacent){
+                if(n.marked == false){
+                    n.marked = true;
+                    queue.add(n);
+                }
+            }
+            visit(r);
+        }
+    }
+
+    void dfsR(Node r){
+        if(r == null) return;
+        r.marked = true;
+        visit(r);
+        for(Node n : r.adjacent){
+            if(n.marked == false){
+                dfsR(n);
+            }
+        }
+    }
+
+    void dfsR(int index){
+        Node r =nodes[index];
+        dfsR(r);
+    }
+
+    void dfsR(){
+        dfsR(0);
+    }
+}
+public class DFSBFS {
+    public static void main(String []args){
+        Graph g = new Graph(9);
+        g.addEdge(0,1);
+        g.addEdge(1,2);
+        g.addEdge(1,3);
+        g.addEdge(2,4);
+        g.addEdge(2,3);
+        g.addEdge(3,4);
+        g.addEdge(3,5);
+        g.addEdge(5,6);
+        g.addEdge(5,7);
+        g.addEdge(6,8);
+        g.dfs(3);
+
+    }
+}
