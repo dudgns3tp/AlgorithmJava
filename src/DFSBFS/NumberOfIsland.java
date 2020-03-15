cf94ded3e83eec26b065cb4480e4d90622fef3b1
Choi Young Hoon <c_yh0327@naver.com>
2020-03-15 22:05:51 +0900

    bfs

--- src/DFSBFS/NumberOfIsland.java
+++ src/DFSBFS/NumberOfIsland.java
@@ -1,5 +1,8 @@
 package DFSBFS;
 
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class NumberOfIsland {
     public static void main(String []args){
         char[][] grid= {
@@ -9,10 +12,32 @@ public class NumberOfIsland {
                 {'0','0','0','0','1'}
         };
         NumberOfIsland a = new NumberOfIsland();
-        System.out.println(a.solve(grid));
+        //System.out.println(a.solveDFS(grid));
+        System.out.println(a.solveBFS(grid));
     }
 
-    public int solve(char[][] grid){
+    int [][] dirs= {{-1,0}, {1,0}, {0,1},{0,-1}};
+
+    public int solveBFS(char[][] grid){
+        System.out.println("----bfs----");
+        //1. error
+        if(grid.length==0|| grid == null|| grid[0].length==0) return 0;
+
+        //2.
+        int count = 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j<grid[i].length; j++){
+                if(grid[i][j]=='1') {
+                    count++;
+                    bfs(grid, i, j);
+                }
+
+            }
+        }
+        return count;
+    }
+
+    public int solveDFS(char[][] grid){
         print(grid);
 
         //1 error managing
@@ -28,11 +53,30 @@ public class NumberOfIsland {
                 }
 
             }
-            System.out.println();
         }
         System.out.println("=============after==============");
         print(grid);
         return count;
+    }
+
+    public void bfs(char[][] grid, int x, int y){
+        Queue<int []> queue = new LinkedList<>();
+        queue.offer(new int[] {x,y});
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            int[] point = queue.poll();
+            for(int i = 0; i<size; i++){
+                for(int[] dir: dirs){
+                    int x1 = point[0] + dir[0];
+                    int y1 = point[1] + dir[1];
+                    if(x1>=0 && y1>=0 && x1<grid.length && y1<grid[0].length && grid[x1][y1] == '1'){
+                        System.out.println("x1:"+x1+" y1:"+y1);
+                        grid[x1][y1] = '0';
+                        queue.offer(new int[] {x1,y1});
+                    }
+                }
+            }
+        }
     }
     public void dfs(char[][] grid,int i, int j){
         //System.out.println("i: "+i+" j "+j);
