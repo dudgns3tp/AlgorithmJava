9f5d39e6ce1b2519bfe0407b9b03987ef2b95c5d
최영훈 <c_yh0327@naver.com>
2020-05-26 15:19:54 +0900

    algo

---
+++ src/backjoon/Q10867.java
@@ -0,0 +1,25 @@
+package backjoon;
+
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Q10867 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Set<Integer> set = new TreeSet<Integer>();
+        while(n-->0) {
+            set.add(sc.nextInt());
+        }
+
+        Iterator it = set.iterator();
+
+        while(it.hasNext()) {
+            System.out.print(it.next() + " ");
+        }
+    }
+}
